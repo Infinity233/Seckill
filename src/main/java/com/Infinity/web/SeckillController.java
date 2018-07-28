1fc137bcc9d576c23d8ffacccb8fb27bdb33a344
Infinity233 <734750935@qq.com>
2018-07-28 22:37:26 +0800

    Controller接口

---
+++ src/main/java/com/Infinity/web/SeckillController.java
@@ -0,0 +1,107 @@
+package com.Infinity.web;
+
+import com.Infinity.dto.Exposer;
+import com.Infinity.dto.SeckillExecution;
+import com.Infinity.entity.Seckill;
+import com.Infinity.enums.SeckillStatEnum;
+import com.Infinity.exception.RepeatKillException;
+import com.Infinity.exception.SeckillCloseException;
+import com.Infinity.exception.SeckillException;
+import com.Infinity.service.SeckillService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/seckill") // url: /模块/资源/{id}/细分
+public class SeckillController {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private SeckillService seckillService;
+
+    @Autowired
+    public SeckillController(SeckillService seckillService) {
+        this.seckillService = seckillService;
+    }
+
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list(Model model) {
+
+        List<Seckill> seckillList = seckillService.getSeckillList();
+        model.addAttribute("list", seckillList);
+
+        return "list";
+    }
+
+    @RequestMapping(value = "/{seckillId}/detail", method = RequestMethod.GET)
+    public String detail(@PathVariable("seckillId") Long seckillId, Model model) {
+        if (seckillId == null) {
+            return "redirect:/seckill/list";
+        }
+
+        Seckill seckill = seckillService.getById(seckillId);
+        if (seckill == null) {
+            return "forward:/seckill/list";
+        }
+
+        model.addAttribute("seckill", seckill);
+        return "detail";
+    }
+
+    @RequestMapping(value = "/{seckillId}/exposer", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public SeckillResult<Exposer> exposer(Long sedckillId) {
+        SeckillResult<Exposer> result = null;
+
+        try {
+            Exposer exposer = seckillService.exportSeckillUrl(sedckillId);
+            result = new SeckillResult<>(true, exposer);
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            result = new SeckillResult<>(false, e.getMessage());
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/{seckillId}/{md5}/execution", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public SeckillResult<SeckillExecution> execute(Long seckillId, String md5, @CookieValue(value = "killPhone", required = false) Long phone) {
+
+        if (phone == null) {
+            return new SeckillResult<>(false, "未注册");
+        }
+
+        SeckillResult<SeckillExecution> result = null;
+        try {
+            SeckillExecution seckillExecution = seckillService.executeSeckill(seckillId, phone, md5);
+            result = new SeckillResult<>(true, seckillExecution);
+        } catch (RepeatKillException e) {
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStatEnum.REPEAT_KILL);
+            return new SeckillResult<>(false, execution);
+        } catch (SeckillCloseException e) {
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStatEnum.END);
+            new SeckillResult<>(false, execution);
+        } catch (SeckillException e) {
+            SeckillExecution execution = new SeckillExecution(seckillId, SeckillStatEnum.INNER_ERROR);
+            return new SeckillResult<>(false, execution);
+        }
+
+        return result;
+    }
+
+    @RequestMapping(value = "/time/now", method=RequestMethod.GET)
+    @ResponseBody
+    public SeckillResult<Long> time() {
+        Date now = new Date();
+
+        return new SeckillResult<>(true, now.getTime());
+    }
+}
